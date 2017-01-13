a02dc29429fb5389aa627eef183e91ab37ffff61
AGWard <anthony.ward.data@gmail.com>
2017-01-13 19:20:15 -0400

    Added Navigation Controller & Collection View Controller programmatically

---
+++ News/HomeController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeController.swift
+//  News
+//
+//  Created by Anthony Ward on 1/13/17.
+//  Copyright © 2017 AppCo. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UICollectionViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.backgroundColor = .green
+    }
+}
